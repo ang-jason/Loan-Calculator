e91416186cc053fd329f0812c881c46abbe2ca3d
ang-jason <angjason896@gmail.com>
2021-09-07 17:56:30 +0800

    edited 3 files already + baseline backup

--- app/analyse_schedule.py
+++ app/analyse_schedule.py
@@ -1,24 +1,23 @@
-
 import pandas as pd
 
 
 # Class definition
 class AnalyseSchedule():
-    '''
-    This AnalyseSchedule Class is to perform table top analytics on the schedule (dataframe)
-  
-    A Basic getter and setter functions to parameters and the intended data types (dataframe)
-
+    ''' 
+    This AnalyseSchedule Class is to perform table top 
+    analytics on the schedule (dataframe)
+    A Basic getter and setter functions to parameters 
+    and the intended data types (dataframe)
     '''
     # Attributes:
-    def __init__(self,df):
+    def __init__(self, df):
         self.df = df
 
     # property getter
     @property
     def df(self):
         return self._df
-    
+
     # property setter
     @df.setter
     def df(self, df_value):
@@ -26,70 +25,70 @@ class AnalyseSchedule():
             self._df = df_value
     ''' 
     this function is for the summary of the loan
-    
     returns total payments, principal, interest of the entire loan
-
     '''
     def total_ppi(self):
-        total_payment=sum(self._df['Payment'])
-        total_principal=sum(self._df['Principal'])
-        total_interest=sum(self._df['Interest'])
-        
-        return total_payment,total_principal,total_interest
+        total_payment = sum(self._df['Payment'])
+        total_principal = sum(self._df['Principal'])
+        total_interest = sum(self._df['Interest'])
+        return total_payment, total_principal, total_interest
 
-    # this function is analysis of how much interst vs the principal amount returns ratio in percentage
+    # this function is analysis of how much interst vs 
+    # the principal amount returns ratio in percentage
     def interest_to_principal(self):
 
-        total_payment,total_principal,total_interest=self.total_ppi()
-        ratio=total_interest/total_principal*100
+        total_payment, total_principal, total_interest = self.total_ppi()
+        ratio = total_interest/total_principal*100
         return ratio
 
-    # this function is analysis of how much payment vs the loan amount returns ratio in percentage
+    # this function is analysis of how much payment vs
+    # the loan amount returns ratio in percentage
     def payment_to_loan(self):
 
-        total_payment,total_principal,total_interest=self.total_ppi()
-        loan_size=self._df['StartBalance'].iloc[0]
-        
-        ratio=total_payment/loan_size*100
+        total_payment, total_principal, total_interest = self.total_ppi()
+        loan_size = self._df['StartBalance'].iloc[0]
+        ratio = total_payment/loan_size*100
         return ratio
 
-    # this function is to return specific row (period_row) of the dataframe's schedule
-    def show_schedule_row(self,period_row):
+    # this function is to return specific row (period_row) 
+    # of the dataframe's schedule
+    def show_schedule_row(self, period_row):
 
         return self._df['StartBalance'].iloc[period_row], \
                     self._df['Payment'].iloc[period_row],    \
                     self._df['Principal'].iloc[period_row],  \
                     self._df['Interest'].iloc[period_row],   \
                     self._df['EndBalance'].iloc[period_row]
-                    
-    # this function to return the quick view of monthly payment and the number of times the payments to be made
+    # this function to return the quick view of 
+    # monthly payment and the number of times the payments to be made
+
     def show_payments_brief(self):
         answer = self._df['Payment']
         from collections import Counter
-    
-        return Counter(answer).keys(),Counter(answer).values()
-        
+        return Counter(answer).keys(), Counter(answer).values()
+
     # this function to return individual colums of list to pipe into chartjs 
     def show_yearly_brief(self):
-        yearly=len(self._df)//12
-        data_sbalance=[]
-        data_payment=[]
-        data_principal=[]
-        data_interest=[]
+        yearly = len(self._df)//12
+        data_sbalance = []
+        data_payment = []
+        data_principal = []
+        data_interest = []
         for i in range(yearly):
             # print(self.show_schedule_row(i*12))
-            #capture starting balance, payments and principal, interest? more meaning?
+            # capture starting balance, payments & principal & interest? 
+            # more meaning?
             data_sbalance.append(self.show_schedule_row(i*12)[0])
             data_payment.append(self.show_schedule_row(i*12)[1])
             data_principal.append(self.show_schedule_row(i*12)[2])
-            data_interest.append(self.show_schedule_row(i*12)[3])  
-            
-        yearly_labels=[ x+1 for x in range(yearly)]
+            data_interest.append(self.show_schedule_row(i*12)[3])     
+        yearly_labels = [x+1 for x in range(yearly)]
         # print(yearly_labels)
         data_col_names = [self._df.columns[x] for x in range(len(self._df.columns)) if x < 4]
         # print(data_col_names)
-        return yearly_labels,data_col_names,data_sbalance,data_payment,data_principal,data_interest
-        
+        return yearly_labels, data_col_names, data_sbalance, \
+            data_payment, data_principal, data_interest
+
     ''' 
     this function is to analytical table top summary of the loan
     (this basic format should be as shown below)
@@ -108,44 +107,37 @@ class AnalyseSchedule():
     def loan_tabletop_brief(self):
 
         # this is for total_payment,total_principal,total_interest
-        total_payment,total_principal,total_interest=self.total_ppi()
+        total_payment, total_principal, total_interest = self.total_ppi()
 
         # this is for payment to loan ratio
-        ratio=self.payment_to_loan()
+        ratio = self.payment_to_loan()
 
         # this is for assoicated monthly payments
-        mthly_payments,mthly_period=self.show_payments_brief()
-        mthly_payments=['$ {0:.{1}f}'.format(x, 2) for x in mthly_payments]
+        mthly_payments, mthly_period = self.show_payments_brief()
+        mthly_payments = ['$ {0:.{1}f}'.format(x, 2) for x in mthly_payments]
         # print("This is year 2 loan period")
         # print("This is year 3 loan period")
         # print("This is year 5 loan period")
-        byear2,byear3,byear5=self.show_schedule_row(2*12-1),    \
-                                self.show_schedule_row(3*12-1), \
-                                self.show_schedule_row(5*12-1)
+        byear2, byear3, byear5 = self.show_schedule_row(2*12-1),    \
+            self.show_schedule_row(3*12-1), \
+            self.show_schedule_row(5*12-1)
 
         # pd.options.display.float_format = '{:,.2f}'.format
-        resultant={
+        resultant = {
             "Total interest payable": f'$ {total_interest:.0f}',
             "Total principal": f'$ {total_principal:.0f}',
             "Total payable": f'$ {total_payment:.0f}',
-            "Total payments to loan ratio":f'{ratio:.1f} %',
+            "Total payments to loan ratio": f'{ratio:.1f} %',
             "Associated monthly payments": ' '.join(mthly_payments),
             "Principal after Year 2": f'$ {byear2[-1]:.0f}',
             "Principal after Year 3": f'$ {byear3[-1]:.0f}',
             "Principal after Year 5": f'$ {byear5[-1]:.0f}',
             }
-       
         return resultant
-        
+
     def __str__(self):
         print(f"-"*50)
-        display_txt="This is your schedule: \n"
+        display_txt = "This is your schedule: \n"
         print(f"-"*50)
-        display_txt+=f"${self._df}"
+        display_txt += f"${self._df}"
         return display_txt
-        
-        
-
-
-
-    
--- app/interest_rate_market.py
+++ app/interest_rate_market.py
@@ -1,23 +1,25 @@
 class InterestRateMarket:
     '''
-    
-    This is InterestRateMarket Class that take in interest rate (annual), year_length (optional) and month_length(optional)
-    which convert the interest rate to monthly interest rate market convention year_length (12 months)
-    or custom conventions which you can specify year_length (365,360) and month_length (30)
-    
-    This is a very refine class configuration which caters to different market conventions if there is any. However of what I known have been written to default mode of (12 months)
-    
+    This is InterestRateMarket Class that take in interest rate (annual), 
+    year_length (optional) and month_length(optional)
+    which convert the interest rate to monthly interest rate 
+    market convention year_length (12 months)
+    or custom conventions which you can specify 
+    year_length (365,360) and month_length (30)
+    This is a very refine class configuration which caters 
+    to different market conventions if there is any. 
+    However of what I known have been written to default mode of (12 months)
     '''
     def __init__(self, given_annual_rate, year_length=12, month_length=0):
         self.given_annual_rate = given_annual_rate/100.0
         self.year_length = year_length
-        self.month_length= month_length
+        self.month_length = month_length
 
     # property getter
     @property
     def given_annual_rate(self):
         return self._given_annual_rate
-    
+
     # property setter
     @given_annual_rate.setter
     def given_annual_rate(self, value):
@@ -28,7 +30,7 @@ class InterestRateMarket:
     @property
     def year_length(self):
         return self._year_length
-    
+
     # property setter
     @given_annual_rate.setter
     def year_length(self, value):
@@ -39,36 +41,29 @@ class InterestRateMarket:
     @property
     def month_length(self):
         return self._month_length
-    
+
     # property setter
     @month_length.setter
     def month_length(self, value):
         if isinstance(value, int) and value != "":
             self._month_length = value
 
-
     @property
     def compute_monthly(self):
-        if self._year_length>0 and self._month_length>0:
+        if self._year_length > 0 and self._month_length > 0:
             return float(self._given_annual_rate/self._year_length*self._month_length)
-        
         else:
-        # print(given_annual_rate)
+            # print(given_annual_rate)
             return float(self._given_annual_rate/self._year_length)
-
 
     # def __div__(self, other):
     #     return self/other
 
-
     def __str__(self):
         print(f"-"*50)
-        return (f"Given Rate (Annual): {self._given_annual_rate}; Year Length: {self._year_length}; Month Length: {self._month_length} Monthly Rate: {self.compute_monthly:.5f}" )
+        return (f"Given Rate (Annual): {self._given_annual_rate}; Year Length: {self._year_length}; Month Length: {self._month_length} Monthly Rate: {self.compute_monthly:.5f}")
 
-
-
-
-##### TESTING
+# TESTING
 
 # testA=InterestRateMarket(1.39)
 # print(testA)
@@ -76,14 +71,11 @@ class InterestRateMarket:
 # print(testA.compute_monthly,type(testA.compute_monthly)) ### 0.00115833333333333
 # assert round(testA.compute_monthly,10) == round(0.00115833333333333,10)
 
-
-
 # testB=InterestRateMarket(1.39,365,30)
 # print(testB)
 # print(testB.given_annual_rate,type(testB.given_annual_rate))
 # print(testB.compute_monthly,type(testB.compute_monthly)) ### 0.00114246575342466
 # assert round(testB.compute_monthly,10) == round(0.00114246575342466,10)
-
 
 # # 0.00115833333333333
 # testC=InterestRateMarket(1.39,360,30)
